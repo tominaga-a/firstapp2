42cd8c0b459eb44a6e384c5b43612312729ba111
LAKEEL\tominaga-a <tominaga-a@lakeel.com>
2026-02-17 17:43:07 +0900

    ルーティングの実装

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+// ルーティングを読み込む
+const routers = require("./routes");
+app.use(routers);
+
+
 // localhost:3000で起動
 app.listen(3000, () => {
     console.log("Listening on localhost port 3000");
