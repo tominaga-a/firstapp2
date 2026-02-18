5613f6f55748fac7d4e5adc53fa0dae9dad84417
LAKEEL\tominaga-a <tominaga-a@lakeel.com>
2026-02-18 15:40:34 +0900

    新規投稿ページの実装

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const app = express();
 
-//テンプレートエンジンにEJS使用の設定
-app.set("view engine", "ejs");
+// テンプレートエンジンにEJS使用するように設定
+app.set("view engine", "ejs")
 
+// publicフォルダ内のファイルを、を静的ファイルとして扱うよう設定
 app.use("/public", express.static("public"));
 
 // ルーティングを読み込む
 const routers = require("./routes");
 app.use(routers);
 
-
 // localhost:3001で起動
 app.listen(3001, () => {
     console.log("Listening on localhost port 3001");
 });
-
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-
-
 //サーバーサイドの処理を読み込み
 const allPosts = require('../controllers/posts/allPosts');
-
-
-
+const createGet = require('../controllers/posts/createGet');
 
 //リクエストに対する実行処理を定義
 router.get('/posts', allPosts);               //投稿一覧表示
+router.get('/posts/create', createGet);       //投稿画面表示
 
 module.exports = router;
