0c5cca85f858cab4f4780d9acedbf0da7b4cbc73
LAKEEL\tominaga-a <tominaga-a@lakeel.com>
2026-02-18 15:20:02 +0900

    投稿一覧表示の実装

---
+++ controllers/posts/allPosts.js
@@ -0,0 +1,9 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+
+
+module.exports = async (req, res) => {
+  const posts = await prisma.post.findMany();
+  res.render("posts/allPosts", {posts});
+};
--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
 
+//テンプレートエンジンにEJS使用の設定
+app.set("view engine", "ejs");
+
+app.use("/public", express.static("public"));
+
 // ルーティングを読み込む
 const routers = require("./routes");
 app.use(routers);
 
 
-// localhost:3000で起動
-app.listen(3000, () => {
-    console.log("Listening on localhost port 3000");
+// localhost:3001で起動
+app.listen(3001, () => {
+    console.log("Listening on localhost port 3001");
 });
+
